31bae8a78f996977995e3b195f487fa5ae23d316
Ford, Johnta <johnta.ford+corewell@corewellhealth.org>
2025-09-07 16:17:20 -0400

    Updating Tic Tac Toe Program to reduce redundancy by adding loops

--- Tic-Tac-Toe/src/App.java
+++ Tic-Tac-Toe/src/App.java
@@ -3,158 +3,55 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    int col;
-    int row;
-
-    Scanner input = new Scanner(System.in);
-
+    
     String[][] board = {{" - ", " - ", " - "},
                         {" - ", " - ", " - "},
-                        {" - ", " - ", " - "}};  
-
-    System.out.println("\t" + Arrays.deepToString(board[0]));
-    System.out.println("\t" + Arrays.deepToString(board[1]));
-    System.out.println("\t" + Arrays.deepToString(board[2]) + "\n");
-
-    System.out.println("X - Select row  (1-3) & select column (1 -3 )");
-    System.out.println("separated by a space: ");
-
-    col = input.nextInt();
-    row = input.nextInt();
-
-   if (board[row - 1][col - 1].equals("-"))
-        board[row - 1][col - 1] = " x ";
-
-        else { 
-            System.out.println(" Sorry this spot is taken.");
-        }
-
-    System.out.println("\t" + Arrays.deepToString(board[0]));
-    System.out.println("\t" + Arrays.deepToString(board[1]));
-    System.out.println("\t" + Arrays.deepToString(board[2]) + "\n");
-
-    // O's 1st turn
-    System.out.print("O - Select row (1-3) & select column (1-3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " O ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
-    
-    // X's 2nd turn
-    System.out.print("X - Select row (1 - 3) & select column (1 - 3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " X ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
-  
- 
-    // O's 2nd turn
-    System.out.print("O - Select row (1-3) & select column (1-3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " O ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");  
- 
-    // X's 3rd turn
-    System.out.print("X - Select row (1 - 3) & select column (1 - 3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " X ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
+                        {" - ", " - ", " - "}};
+        
     System.out.println("\t" + Arrays.toString(board[0]));
     System.out.println("\t" + Arrays.toString(board[1]));
     System.out.println("\t" + Arrays.toString(board[2]) + "\n");
         
-    // O's 3rd turn
-    System.out.print("O - Select row (1-3) & select column (1-3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " O ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
+    Scanner input = new Scanner(System.in);
+    // Initialize col & row so that the input validation works 
+    int col = 0;
+    int row = 0;
+    // Variable to keep track of current player
+    char player;
     
-    // X's 4th turn
-    System.out.print("X - Select row (1 - 3) & select column (1 - 3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " X ";
+    // for loop to provide total of 9 turns
+    for(int turn = 0; turn < 9; turn++) {
+      // If an even turn number, player is X
+      if(turn % 2 == 0) {
+        player = 'X';
+      }
+      // Otherwise, odd turns are O's turns
+      else {
+        player = 'O';
+      }
+      // Check if col or row is less than 1 or greater than 3
+      while(col < 1 || col > 3 || row < 1 || row > 3){
+        System.out.print(player + " - Select row (1 - 3) & select column (1 - 3) ");
+        System.out.print("separated by a space: ");
+        row = input.nextInt();
+        col = input.nextInt();
+      }
+      if(board[row - 1][col - 1].equals(" - ")) {
+        // Use player to provide character (X or O)
+        board[row - 1][col - 1] = " " + player + " ";
+        System.out.println("\t" + Arrays.toString(board[0]));
+        System.out.println("\t" + Arrays.toString(board[1]));
+        System.out.println("\t" + Arrays.toString(board[2]) + "\n");
+      }
+      else {
+        // If position already taken, print message 
+        System.out.println("Sorry, that spot is taken.");
+        // Roll loop var back by 1 so that the turn repeats
+        turn--;
+      }
+      // Reset col and row so that validation loop runs correctly
+      col = 0;
+      row = 0;
     }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
- 
-    // O's 4th turn
-    System.out.print("O - Select row (1-3) & select column (1-3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " O ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");  
- 
-    // X's 5th turn
-    System.out.print("X - Select row (1 - 3) & select column (1 - 3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    if(board[row - 1][col - 1].equals(" - ")) {
-      board[row - 1][col - 1] = " X ";
-    }
-    else {
-      System.out.println("Sorry, that spot is taken.");
-    }
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
-  
-
-
-    }
+  }
 }
